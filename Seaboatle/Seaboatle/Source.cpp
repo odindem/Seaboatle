ef6f147360a7e273629d688d887f2b74ce861f7e
Odindem <odindem@me.com>
2021-04-08 18:12:49 +0300

    Додав к3 та к2

--- Seaboatle/Seaboatle/Source.cpp
+++ Seaboatle/Seaboatle/Source.cpp
@@ -1,32 +1,37 @@
 #include<iostream>
 #include<time.h>
 using namespace std;
+
 int main()
 {
 	srand(time(NULL));
-	char sea[11][11];
-	char sea2[11][11];
+	char sea[12][12];
+	char sea2[12][12];
 	int k1 = 0, k2 = 0, k3 = 0, k4 = 0;
-	for (int i = 0; i < 11; i++)
-		for (int j = 0; j < 11; j++)
+	for (int i = 0; i < 12; i++)
+		for (int j = 0; j < 12; j++)
 		{
 			sea[i][j] = 0;
 			sea2[i][j] = 0;
 		}
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 11; i++)
 	{
-			sea[i][10] = 48 + i;
-			sea[10][i] = 65 + i;
-			sea2[i][10] = 48 + i;
-			sea2[10][i] = 65 + i;
+			sea[i][0] = 8;
+			sea[0][i] = 8;
+			sea2[i][0] = 8;
+			sea2[0][i] = 8;
+			sea[i][11] = 47 + i;
+			sea[11][i] = 128 + i;
+			sea2[i][11] = 47 + i;
+			sea2[11][i] = 128 + i;
 	}
 	
 	while (k4 != 1)
 	{
 		int rando = rand() % 4 + 1;
-		int x = rand() % 10;
-		int y = rand() % 10;
+		int x =rand() % 9+1;
+		int y = rand() % 9+1;
 
 
 
@@ -124,33 +129,264 @@ int main()
 			}
 		}
 	}
-
-	//while (k3 != 1)
-	//{
-	//	int rando = /*rand() % 4 + */1;
-	//	int x = rand() % 10;
-	//	int y = rand() % 10;
-	//	if (rando==1)
-	//	{
-	//		int tr = 0;
-	//		for(int i=x;i<;i++, tempx++)
-	//			for (int j = 0, int tempy=0; j < 11; j++, tempy++)
-	//			{
-	//				
-	//				tr += sea2[(x - 1) + tempx][(y - 1) + tempy];
-
-	//			}
-	//		if (tr != 0)
-	//		{
-	//			for (int i = 0; i < 3; i++)
-	//				sea2[x+i][y] = 178;
-	//			k3++;
-	//		}
-	//	}
-	//}
-	for (int i = 0; i < 11; i++)
+	for (int i = 0; i < 12; i++)
 	{
-		for (int j = 0; j < 11; j++)
+		for (int j = 0; j < 12; j++)
+		{
+			/*cout << sea[i][j];*/
+			cout << sea2[i][j];
+
+		}
+		cout << endl;
+	}
+	
+	while (k3 != 2)
+	{
+		int tr = 0;
+		int rando = rand() % 2+1;
+		int x = rand() % 10+1;
+		int y = rand() % 10+1;
+		cout << endl  << x << "\t" << y << endl;
+		if (rando==1)
+		{
+			
+				for (int i = x - 1; i < x + 4; i++)
+				{
+					for (int j = y - 1; j < y + 2; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+			
+			if (tr == 0)
+			{
+				for (int i = 0; i < 3; i++)
+					sea2[x+i][y] = 178;
+				k3++;
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x - 1; i < x + 2; i++)
+				{
+					for (int j = y - 1; j < y + 4; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 3; i++)
+						sea2[x ][y+i] = 178;
+					k3++;
+				}
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x + 1; i > x - 2; i--)
+				{
+					for (int j = y + 1; j > y - 4; j--)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 3; i++)
+						sea2[x][y + i] = 178;
+					k3++;
+				}
+			}
+		}
+		else if (rando == 2)
+		{
+			for (int i = x + 1; i > x - 4; i--)
+			{
+				for (int j = y + 1; j > y - 2; j--)
+				{
+					unsigned char f = sea2[i][j];
+					tr += (int)f;
+				}
+			}
+			if (tr == 0)
+			{
+				for (int i = 0; i < 3; i++)
+					sea2[x - i][y] = 178;
+				k3++;
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x + 1; i > x - 2; i--)
+				{
+					for (int j = y + 1; j > y - 4; j--)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 3; i++)
+						sea2[x][y + i] = 178;
+					k3++;
+				}
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x - 1; i < x + 2; i++)
+				{
+					for (int j = y - 1; j < y + 4; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 3; i++)
+						sea2[x][y + i] = 178;
+					k3++;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			/*cout << sea[i][j];*/
+			cout << sea2[i][j];
+
+		}
+		cout << endl;
+	}
+	cout << "==============================" << endl;
+	while (k2 != 3)
+	{
+		int tr = 0;
+		int rando = rand() % 2 + 1;
+		int x = rand() % 10 + 1;
+		int y = rand() % 10 + 1;
+		cout << endl << x << "\t" << y << endl;
+		if (rando == 1)
+		{
+
+			for (int i = x - 1; i < x + 3; i++)
+			{
+				for (int j = y - 1; j < y + 1; j++)
+				{
+					unsigned char f = sea2[i][j];
+					tr += (int)f;
+				}
+			}
+
+			if (tr == 0)
+			{
+				for (int i = 0; i < 2; i++)
+					sea2[x + i][y] = 178;
+				k2++;
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x - 1; i < x + 1; i++)
+				{
+					for (int j = y - 1; j < y + 3; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 2; i++)
+						sea2[x][y + i] = 178;
+					k2++;
+				}
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x + 1; i > x - 1; i--)
+				{
+					for (int j = y + 1; j > y - 3; j--)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 2; i++)
+						sea2[x][y + i] = 178;
+					k2++;
+				}
+			}
+		}
+		else if (rando == 2)
+		{
+			for (int i = x + 1; i > x - 3; i--)
+			{
+				for (int j = y + 1; j > y - 1; j--)
+				{
+					unsigned char f = sea2[i][j];
+					tr += (int)f;
+				}
+			}
+			if (tr == 0)
+			{
+				for (int i = 0; i < 2; i++)
+					sea2[x - i][y] = 178;
+				k2++;
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x + 1; i > x - 1; i--)
+				{
+					for (int j = y + 1; j > y - 3; j--)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 2; i++)
+						sea2[x][y + i] = 178;
+					k2++;
+				}
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x - 1; i < x + 1; i++)
+				{
+					for (int j = y - 1; j < y + 3; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 2; i++)
+						sea2[x][y + i] = 178;
+					k2++;
+				}
+			}
+		}
+	}
+	
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
 		{
 			/*cout << sea[i][j];*/
 			cout << sea2[i][j];

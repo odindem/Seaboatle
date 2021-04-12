8718d4418764be476521fef33aede9233154d7c6
Odindem <odindem@me.com>
2021-04-12 23:27:17 +0300

    всі кораблі є

--- Seaboatle/Seaboatle/Source.cpp
+++ Seaboatle/Seaboatle/Source.cpp
@@ -5,13 +5,16 @@ using namespace std;
 int main()
 {
 	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+	wcout.imbue(locale(".1251"));
+	wcin.imbue(locale(".866"));
 	char sea[12][12];
 	char sea2[12][12];
 	int k1 = 0, k2 = 0, k3 = 0, k4 = 0;
 	for (int i = 0; i < 12; i++)
 		for (int j = 0; j < 12; j++)
 		{
-			sea[i][j] = 0;
+			sea[i][j] = 15;
 			sea2[i][j] = 0;
 		}
 
@@ -22,10 +25,12 @@ int main()
 			sea2[i][0] = 8;
 			sea2[0][i] = 8;
 			sea[i][11] = 47 + i;
-			sea[11][i] = 128 + i;
+			sea[11][i] = 65 + i;
 			sea2[i][11] = 47 + i;
-			sea2[11][i] = 128 + i;
+			sea2[11][i] = 65 + i;
 	}
+	sea[11][10] = 0;
+	sea[11][11] = 0;
 	
 	while (k4 != 1)
 	{
@@ -129,16 +134,7 @@ int main()
 			}
 		}
 	}
-	for (int i = 0; i < 12; i++)
-	{
-		for (int j = 0; j < 12; j++)
-		{
-			/*cout << sea[i][j];*/
-			cout << sea2[i][j];
-
-		}
-		cout << endl;
-	}
+	
 	
 	while (k3 != 2)
 	{
@@ -146,7 +142,7 @@ int main()
 		int rando = rand() % 2+1;
 		int x = rand() % 10+1;
 		int y = rand() % 10+1;
-		cout << endl  << x << "\t" << y << endl;
+		
 		if (rando==1)
 		{
 			
@@ -256,30 +252,20 @@ int main()
 			}
 		}
 	}
-	for (int i = 0; i < 12; i++)
-	{
-		for (int j = 0; j < 12; j++)
-		{
-			/*cout << sea[i][j];*/
-			cout << sea2[i][j];
-
-		}
-		cout << endl;
-	}
-	cout << "==============================" << endl;
+	
 	while (k2 != 3)
 	{
 		int tr = 0;
 		int rando = rand() % 2 + 1;
 		int x = rand() % 10 + 1;
 		int y = rand() % 10 + 1;
-		cout << endl << x << "\t" << y << endl;
+		
 		if (rando == 1)
 		{
 
 			for (int i = x - 1; i < x + 3; i++)
 			{
-				for (int j = y - 1; j < y + 1; j++)
+				for (int j = y - 1; j < y + 2; j++)
 				{
 					unsigned char f = sea2[i][j];
 					tr += (int)f;
@@ -295,7 +281,7 @@ int main()
 			else if (tr != 0)
 			{
 				tr = 0;
-				for (int i = x - 1; i < x + 1; i++)
+				for (int i = x - 1; i < x + 2; i++)
 				{
 					for (int j = y - 1; j < y + 3; j++)
 					{
@@ -313,7 +299,7 @@ int main()
 			else if (tr != 0)
 			{
 				tr = 0;
-				for (int i = x + 1; i > x - 1; i--)
+				for (int i = x + 1; i > x - 2; i--)
 				{
 					for (int j = y + 1; j > y - 3; j--)
 					{
@@ -333,7 +319,7 @@ int main()
 		{
 			for (int i = x + 1; i > x - 3; i--)
 			{
-				for (int j = y + 1; j > y - 1; j--)
+				for (int j = y + 1; j > y - 2; j--)
 				{
 					unsigned char f = sea2[i][j];
 					tr += (int)f;
@@ -348,7 +334,7 @@ int main()
 			else if (tr != 0)
 			{
 				tr = 0;
-				for (int i = x + 1; i > x - 1; i--)
+				for (int i = x + 1; i > x - 2; i--)
 				{
 					for (int j = y + 1; j > y - 3; j--)
 					{
@@ -366,7 +352,7 @@ int main()
 			else if (tr != 0)
 			{
 				tr = 0;
-				for (int i = x - 1; i < x + 1; i++)
+				for (int i = x - 1; i < x + 2; i++)
 				{
 					for (int j = y - 1; j < y + 3; j++)
 					{
@@ -384,19 +370,148 @@ int main()
 		}
 	}
 	
-	for (int i = 0; i < 12; i++)
+	while (k1 != 4)
 	{
-		for (int j = 0; j < 12; j++)
-		{
-			/*cout << sea[i][j];*/
-			cout << sea2[i][j];
-
-		}
-		cout<<endl;
-	}
+		int tr = 0;
+		int rando = rand() % 2 + 1;
+		int x = rand() % 10 + 1;
+		int y = rand() % 10 + 1;
 		
-	
-			
+		if (rando == 1)
+		{
+
+			for (int i = x - 1; i < x + 2; i++)
+			{
+				for (int j = y - 1; j < y + 2; j++)
+				{
+					unsigned char f = sea2[i][j];
+					tr += (int)f;
+				}
+			}
+
+			if (tr == 0)
+			{
+				for (int i = 0; i < 1; i++)
+					sea2[x + i][y] = 178;
+				k1++;
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x - 1; i < x + 2; i++)
+				{
+					for (int j = y - 1; j < y + 2; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 1; i++)
+						sea2[x][y + i] = 178;
+					k1++;
+				}
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x + 1; i > x - 2; i--)
+				{
+					for (int j = y + 1; j > y - 2; j--)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 1; i++)
+						sea2[x][y + i] = 178;
+					k1++;
+				}
+			}
+		}
+		else if (rando == 2)
+		{
+			for (int i = x + 1; i > x - 2; i--)
+			{
+				for (int j = y + 1; j > y - 2; j--)
+				{
+					unsigned char f = sea2[i][j];
+					tr += (int)f;
+				}
+			}
+			if (tr == 0)
+			{
+				for (int i = 0; i < 1; i++)
+					sea2[x - i][y] = 178;
+				k1++;
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x + 1; i > x - 2; i--)
+				{
+					for (int j = y + 1; j > y - 2; j--)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 1; i++)
+						sea2[x][y + i] = 178;
+					k1++;
+				}
+			}
+			else if (tr != 0)
+			{
+				tr = 0;
+				for (int i = x - 1; i < x + 2; i++)
+				{
+					for (int j = y - 1; j < y + 2; j++)
+					{
+						unsigned char f = sea2[i][j];
+						tr += (int)f;
+					}
+				}
+				if (tr == 0)
+				{
+					for (int i = 0; i < 1; i++)
+						sea2[x][y + i] = 178;
+					k1++;
+				}
+			}
+		}
+	}
+	cout << "Почнемо гру!\nНатиснiть будь яку клавiшу для початку!" << endl;
+	system("pause");
+	system("CLS");
+	int ship = 20;
+	while (ship>0)
+	{
+		char v;
+		int h, vint;
+		for (int i = 0; i < 12; i++)
+		{
+			for (int j = 0; j < 12; j++)
+			{
+				cout << sea[i][j];
+				/*cout << sea2[i][j];*/
+
+			}
+			cout << endl;
+		}
+		cout << "Ведiть координати!\nБуква->Цифра" << endl;
+		cin >> v >> h;
+		for (int i = 0, int l = 65; i < 10; i++, l++)
+			if (v == l)
+				vint = i;
+		
+
+	}
 	
 	
 
